53bd55258edba3c7f8c4d0cbc08e877a3eb4d8df
jeesmjoseph <78970300+jeesmjoseph@users.noreply.github.com>
2022-05-23 00:37:53 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,67 @@
+// Part #1 Point 1
+// Renamed app.js as server.js which is the main file
+const express = require('express'); 
+const path = require ('path'); 
+// Part #1 Point 2
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+// Part #1 Point 3
+const homeRouter = require('./src/routes/homerouter');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+// Part #2 Point 2
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+
+// Part #2 Point 2
+app.use(cors());
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+// Part #1 Point 5
+app.listen(5000,()=>{
+    console.log("Server Ready on 5000");
+});
